04fcfdbd46e610b9cd5c58e525b9e064306824b1
Kamal Meena <hanshkamalmeena2005@gmail.com>
2025-01-24 23:14:20 +0530

    new commit

--- src/Screens/Home.jsx
+++ src/Screens/Home.jsx
@@ -48,7 +48,7 @@ export default function Home() {
                     <About />
                     <Glimpse />
                     {/* <Sponsorship /> */}
-                    <Contact />
+                    {/* <Contact /> */}
                 </>
             )}
         </>
